ea6c3c200f7650ac4ff461ece5fb2273ce89ad01
조성원 <tjddnjsjo@kakao.com>
2024-01-12 06:51:19 +0900

    feat: add main carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,34 @@
+import Image from 'next/image'
+
+import { AspectRatio } from '@/components/ui/aspect-ratio'
+import { Card } from '@/components/ui/card'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from '@/components/ui/carousel'
+
 export default function Home() {
-  return <div className="text-3xl text-primary">Hello, world!</div>
+  return (
+    <>
+      <Carousel className="w-full">
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <Card>
+                <AspectRatio ratio={16 / 9} className="bg-muted">
+                  <Image
+                    className="rounded-md object-cover"
+                    src={`https://picsum.photos/id/${index}/3200/1800?grayscale`}
+                    alt="image"
+                    fill
+                  />
+                </AspectRatio>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+    </>
+  )
 }
