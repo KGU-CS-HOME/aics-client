a5918eb68a914e632249c3fa152a860e2a3928c8
조성원 <tjddnjsjo@kakao.com>
2024-01-12 15:43:18 +0900

    chore: add @next/mdx

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+const withMDX = require('@next/mdx')()
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '**' }],
   },
 }
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig)
