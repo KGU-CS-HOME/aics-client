503e76dce104ef278f5a488ab9d53d4b38b37b2c
조성원 <tjddnjsjo@kakao.com>
2024-01-12 17:01:01 +0900

    chore: add remark-gfm

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+import createMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: '**' }],
+  },
+}
+
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+})
+
+export default withMDX(nextConfig)
