3a5b3cede5cd8c47f1f7dec543b6ee8e51f2e2fd
조성원 <tjddnjsjo@kakao.com>
2024-01-13 21:56:54 +0900

    feat: create side-menu component

--- src/app/(static)/about/layout.tsx
+++ src/app/(static)/about/layout.tsx
@@ -1,4 +1,5 @@
 import { PageHeader } from '@/components/page-header'
+import { MenuItem, SideMenu } from '@/components/side-menu'
 
 export default function AboutLayout({
   children,
@@ -8,9 +9,14 @@ export default function AboutLayout({
   return (
     <>
       <PageHeader>학부소개</PageHeader>
-      <div className="flex flex-col justify-between px-2 pt-10 lg:flex-row">
-        <div className="">메뉴</div>
-        <div className="w-full max-w-screen-lg">{children}</div>
+      <div className="flex flex-col justify-between gap-12 px-2 pt-10 lg:flex-row lg:items-start">
+        <SideMenu>
+          <MenuItem href="/about">AI컴퓨터공학부</MenuItem>
+          <MenuItem href="/about/cs">컴퓨터공학전공</MenuItem>
+          <MenuItem href="/about/ai">인공지능전공</MenuItem>
+          <MenuItem href="/about/sw">SW안전보안전공</MenuItem>
+        </SideMenu>
+        <div className="w-full p-4">{children}</div>
       </div>
     </>
   )
