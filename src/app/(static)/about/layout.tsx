335a0d84ade06f569929bf972b04e94da3e30833
조성원 <tjddnjsjo@kakao.com>
2024-01-13 21:06:20 +0900

    feat: create page-header component

--- src/app/(static)/about/layout.tsx
+++ src/app/(static)/about/layout.tsx
@@ -1,4 +1,4 @@
-import { Card, CardHeader } from '@/components/ui/card'
+import { PageHeader } from '@/components/page-header'
 
 export default function AboutLayout({
   children,
@@ -7,11 +7,7 @@ export default function AboutLayout({
 }) {
   return (
     <>
-      <Card className="flex h-[300px] flex-col items-center justify-center bg-gradient-to-b from-muted/50 to-muted p-4">
-        <CardHeader>
-          <h1 className="text-5xl font-bold">학부소개</h1>
-        </CardHeader>
-      </Card>
+      <PageHeader>학부소개</PageHeader>
       <div className="flex flex-col justify-between px-2 pt-10 lg:flex-row">
         <div className="">메뉴</div>
         <div className="w-full max-w-screen-lg">{children}</div>
