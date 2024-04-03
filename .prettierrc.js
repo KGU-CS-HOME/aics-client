fbe231047da13c0e6b516a6a8d3eb3621912a2a6
조성원 <tjddnjsjo@kakao.com>
2024-04-03 18:08:16 +0900

    style: format code

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: false,
+  singleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
+
+module.exports = config
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@repo/ui/components/ui/button";
+import { Button } from '@repo/ui/components/ui/button'
 
 export default function Page(): JSX.Element {
   return (
     <main>
       <Button>Hi</Button>
     </main>
-  );
+  )
 }
