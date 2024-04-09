ae1adfa9191d9e1df805d4d290d7778bff95ad7a
조성원 <tjddnjsjo@kakao.com>
2024-04-09 11:00:04 +0900

    refactor: professor id 변경

--- apps/web/data/professors.ts
+++ apps/web/data/professors.ts
@@ -1,5 +1,5 @@
 interface Professor {
-  id: number
+  id: string
   name: string
   contact: string
   email: string
@@ -7,115 +7,115 @@ interface Professor {
 
 const professors: Professor[] = [
   {
-    id: 1,
+    id: 'MTk5MTAwNDI',
     name: '권기현',
     contact: '031-249-9666',
     email: 'khkwon@kyonggi.ac.kr',
   },
   {
-    id: 2,
+    id: 'MTk5NTAwMTg',
     name: '전준철',
     contact: '031-249-9668',
     email: 'jcchun@kyonggi.ac.kr',
   },
   {
-    id: 3,
+    id: 'MTk5NjAwMDQ',
     name: '김인철',
     contact: '031-249-9669',
     email: 'kic@kyonggi.ac.kr',
   },
   {
-    id: 4,
+    id: 'MTk5ODAwMjA',
     name: '김광훈',
     contact: '031-249-9679',
     email: 'kwang@kyonggi.ac.kr',
   },
   {
-    id: 5,
+    id: 'MjAwMDAyNTE',
     name: '이은정',
     contact: '031-249-9671',
     email: 'ejlee@kyonggi.ac.kr',
   },
   {
-    id: 6,
+    id: 'MjAwMzAxNTQ',
     name: '권준희',
     contact: '031-249-9675',
     email: 'kwonjh@kyonggi.ac.kr',
   },
   {
-    id: 7,
+    id: 'MjAwMzAxNTM',
     name: '안진호',
     contact: '031-249-9674',
     email: 'jhahn@kyonggi.ac.kr',
   },
   {
-    id: 8,
+    id: 'MjAwNzAyMTc',
     name: '김남기',
     contact: '031-249-9662',
     email: 'ngkim@kyonggi.ac.kr',
   },
   {
-    id: 9,
+    id: 'MjAwOTAxODA',
     name: '김희열',
     contact: '031-249-9607',
     email: 'heeyoul.kim@kyonggi.ac.kr',
   },
   {
-    id: 10,
+    id: 'MjAxMDAxMTg',
     name: '이병대',
     contact: '031-249-9676',
     email: 'blee@kgu.ac.kr',
   },
   {
-    id: 11,
+    id: 'MjAxMDAxMjA',
     name: '배상원',
     contact: '031-249-9677',
     email: 'swbae@kgu.ac.kr',
   },
   {
-    id: 12,
+    id: 'MjAxNzAwOTc',
     name: '정경용',
     contact: '031-249-1382',
     email: 'kychung@kyonggi.ac.kr',
   },
   {
-    id: 13,
+    id: 'MjAxODAwMzc',
     name: '김도훈',
     contact: '031-249-1364',
     email: 'karmy01@kyonggi.ac.kr',
   },
   {
-    id: 14,
+    id: 'MjAyMzAwNDI',
     name: '안준호',
     contact: '031-249-9731',
     email: 'jha@kyonggi.ac.kr',
   },
   {
-    id: 15,
+    id: 'MjAyNDAwNDc',
     name: '이재흥',
     contact: '031-249-1302',
     email: 'jhlee@kyonggi.ac.kr',
   },
   {
-    id: 16,
+    id: 'MjAyMDAzMTE',
     name: '임현기',
     contact: '031-249-1318',
     email: 'hlim20@kyonggi.ac.kr',
   },
   {
-    id: 17,
+    id: 'MjAyMTAwMTc',
     name: '윤익준',
     contact: '031-249-9642',
     email: 'ijyoon@kyonggi.ac.kr',
   },
   {
-    id: 18,
+    id: 'MjAyMzAwNDE',
     name: '이한용',
     contact: '031-249-9637',
     email: 'caehyl210@kyonggi.ac.kr',
   },
   {
-    id: 19,
+    id: 'MjAyMTAyNzk',
     name: '나관상',
     contact: '031-249-1306',
     email: 'ksna001@kyonggi.ac.kr',
