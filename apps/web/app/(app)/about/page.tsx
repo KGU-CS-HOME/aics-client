218b2c03e55ea32a4c457801391fd96de0da1049
조성원 <tjddnjsjo@kakao.com>
2024-04-09 13:19:10 +0900

    feat: 페이지 타이틀 레이아웃 변경

--- apps/web/app/(app)/about/page.tsx
+++ apps/web/app/(app)/about/page.tsx
@@ -9,25 +9,27 @@ import {
 
 export default function Page(): JSX.Element {
   return (
-    <div className="container flex max-w-screen-2xl flex-col gap-8 pt-4">
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/">홈</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbPage>학부소개</BreadcrumbPage>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
-      <div className="flex flex-col">
-        <h2 className="scroll-m-20 pb-2 text-3xl font-bold tracking-tight">
-          학부 소개
-        </h2>
-        <p className="text-muted-foreground">
-          경기대학교 AI컴퓨터공학부를 소개합니다.
-        </p>
+    <div className="container max-w-screen-2xl">
+      <div className="mb-6 flex flex-col gap-8 py-6">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/">홈</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>학부소개</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+        <div className="flex flex-col">
+          <h2 className="scroll-m-20 pb-2 text-3xl font-bold tracking-tight">
+            학부 소개
+          </h2>
+          <p className="text-muted-foreground">
+            경기대학교 AI컴퓨터공학부를 소개합니다.
+          </p>
+        </div>
       </div>
     </div>
   )
