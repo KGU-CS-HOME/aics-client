cc8582b4f5e27858b473901ff9b1476df820372d
조성원 <tjddnjsjo@kakao.com>
2024-04-09 11:39:48 +0900

    feat: breadcrumb, title 추가

--- apps/web/app/(app)/about/page.tsx
+++ apps/web/app/(app)/about/page.tsx
@@ -1,5 +1,34 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@repo/ui/components/ui/breadcrumb'
+
 export default function Page(): JSX.Element {
   return (
-    <div className="container max-w-screen-2xl">학부소개 페이지입니다.</div>
+    <div className="container flex max-w-screen-2xl flex-col gap-8 pt-4">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">홈</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>학부 소개</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <div className="flex flex-col">
+        <h2 className="scroll-m-20 pb-2 text-3xl font-bold tracking-tight">
+          학부 소개
+        </h2>
+        <p className="text-muted-foreground">
+          경기대학교 AI컴퓨터공학부를 소개합니다.
+        </p>
+      </div>
+    </div>
   )
 }
