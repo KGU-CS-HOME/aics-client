b4142366a9e1443b28477c6b7fd535c380ab52cb
조성원 <tjddnjsjo@kakao.com>
2024-04-09 13:15:45 +0900

    feat: 페이지 타이틀 레이아웃 변경

--- apps/web/app/(app)/professor/page.tsx
+++ apps/web/app/(app)/professor/page.tsx
@@ -11,25 +11,27 @@ import { professors } from '../../../data/professors'
 
 export default function Page(): JSX.Element {
   return (
-    <div className="container flex max-w-screen-2xl flex-col gap-8 pt-4">
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/">홈</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbPage>구성원</BreadcrumbPage>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
-      <div className="flex flex-col">
-        <h2 className="scroll-m-20 pb-2 text-3xl font-bold tracking-tight">
-          교수진 소개
-        </h2>
-        <p className="text-muted-foreground">
-          경기대학교 AI컴퓨터공학부의 교수진을 소개합니다.
-        </p>
+    <div className="container max-w-screen-2xl">
+      <div className="mb-6 flex flex-col gap-8 py-6">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/">홈</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>구성원</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+        <div className="flex flex-col">
+          <h2 className="scroll-m-20 pb-2 text-3xl font-bold tracking-tight">
+            교수진 소개
+          </h2>
+          <p className="text-muted-foreground">
+            경기대학교 AI컴퓨터공학부의 교수진을 소개합니다.
+          </p>
+        </div>
       </div>
       <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
         {professors.map((professor) => (
