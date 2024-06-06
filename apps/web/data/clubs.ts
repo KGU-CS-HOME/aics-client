65bad869ecba434827fbd31cfab10a858dc746ac
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-06-06 19:53:59 +0900

    refactor: add type Club

--- apps/web/data/clubs.ts
+++ apps/web/data/clubs.ts
@@ -1,5 +1,6 @@
 interface Club {
   id: number
+  img?: string
   name: string
   description: string
   site?: string
@@ -9,31 +10,31 @@ const CLUBS: Club[] = [
   {
     id: 1,
     name: 'SSF',
-    description: '',
+    description: '컴퓨터공학부 동아리 SSF입니다.',
     site: 'https://www.ssf.or.kr/',
   },
   {
     id: 2,
     name: 'K.Knock',
-    description: '',
+    description: '보안동아리 K.Knock입니다.',
     site: '',
   },
   {
     id: 3,
     name: '귀뚜라미',
-    description: '',
+    description: '어쿠스틱 밴드 동아리 귀뚜라미입니다.',
     site: '',
   },
   {
     id: 4,
     name: 'C-Lab',
-    description: '',
+    description: '보안동아리 C-Lab입니다.',
     site: 'https://www.clab.page/',
   },
   {
     id: 5,
     name: 'InQ',
-    description: '',
+    description: '컴퓨터공학부 동아리 InQ입니다.',
     site: 'https://sites.google.com/view/inqinq',
   },
 ]
