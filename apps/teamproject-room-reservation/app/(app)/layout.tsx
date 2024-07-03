d2a81ac37c40168584932a6b547573e0c4a55368
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-07-03 21:30:29 +0900

    style: modify Layout

---
+++ apps/teamproject-room-reservation/app/(app)/layout.tsx
@@ -0,0 +1,12 @@
+import { SiteFooter } from '~/components/site-footer'
+import { SiteHeader } from '~/components/site-header'
+
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="relative flex min-h-dvh flex-col">
+      <SiteHeader />
+      <main className="mb-20 flex-1 py-6 lg:mb-28">{children}</main>
+      <SiteFooter />
+    </div>
+  )
+}
