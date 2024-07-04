2b7011cb8fd860a37bd17d2af7dcec394ea4001e
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-07-04 16:45:15 +0900

    refactor: modify css in site-header

--- apps/teamproject-room-reservation/components/site-header.tsx
+++ apps/teamproject-room-reservation/components/site-header.tsx
@@ -8,8 +8,10 @@ function SiteHeader() {
     <header className="border-border/40 bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur">
       <div className="container flex h-14 max-w-screen-2xl items-center md:h-20">
         <Link href="/" className="mr-6 flex items-center">
-          <KGULogo className="mr-2 w-16" />
-          <span className="font-extrabold">팀프로젝트실 예약 시스템</span>
+          <KGULogo className="mr-2 w-12 md:w-16" />
+          <span className="text-sm font-extrabold md:text-base">
+            팀프로젝트실 예약 시스템
+          </span>
         </Link>
         <div className="flex flex-1 items-center justify-end space-x-2">
           <Button asChild>
