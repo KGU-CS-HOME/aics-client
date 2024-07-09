4a5fff37e934755dae93a6fa0db12c9f55eac8b8
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-07-09 15:24:05 +0900

    chore: update next.config.js

--- apps/teamproject-room-reservation/next.config.js
+++ apps/teamproject-room-reservation/next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    dangerouslyAllowSVG: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
   transpilePackages: ['@aics/ui'],
   webpack: (config) => {
@@ -16,9 +7,6 @@ const nextConfig = {
       test: /\.svg$/i,
       use: ['@svgr/webpack'],
     })
-
-    config.resolve.alias.canvas = false
-    config.optimization.minimize = false
 
     return config
   },
